383c35f33182ab14707f8f23d0b0f135c60bfaab
CalvinMagezi <30309553+CalvinMagezi@users.noreply.github.com>
2021-02-01 16:46:42 +0300

    First Draft
    Completed a first draft of the website for Belinda to see

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,140 @@
-/**
-* Template Name: DevFolio - v2.4.0
-* Template URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-(function($) {
+
+(function ($) {
   "use strict";
 
   var nav = $('nav');
   var navHeight = nav.outerHeight();
 
-  $('.navbar-toggler').on('click', function() {
+  $('.navbar-toggler').on('click', function () {
     if (!$('#mainNav').hasClass('navbar-reduce')) {
       $('#mainNav').addClass('navbar-reduce');
     }
   })
 
   // Preloader
-  $(window).on('load', function() {
+  $(window).on('load', function () {
     if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function() {
+      $('#preloader').delay(100).fadeOut('slow', function () {
         $(this).remove();
       });
     }
   });
 
   // Back to top button
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
       $('.back-to-top').fadeIn('slow');
     } else {
       $('.back-to-top').fadeOut('slow');
     }
   });
-  $('.back-to-top').click(function() {
+  $('.back-to-top').click(function () {
     $('html, body').animate({
       scrollTop: 0
     }, 1500, 'easeInOutExpo');
     return false;
   });
 
+  //Chatbox
+  var INDEX = 0;
+  $("#chat-submit").click(function (e) {
+    e.preventDefault();
+    var msg = $("#chat-input").val();
+    if (msg.trim() == '') {
+      return false;
+    }
+    generate_message(msg, 'self');
+    var buttons = [
+      {
+        name: 'Existing User',
+        value: 'existing'
+      },
+      {
+        name: 'New User',
+        value: 'new'
+      }
+    ];
+    setTimeout(function () {
+      generate_message(msg, 'user');
+    }, 1000)
+
+  })
+
+  function generate_message(msg, type) {
+    INDEX++;
+    var str = "";
+    str += "<div id='cm-msg-" + INDEX + "' class=\"chat-msg " + type + "\">";
+    str += "          <span class=\"msg-avatar\">";
+    str += "            <img src=\"https:\/\/image.crisp.im\/avatar\/operator\/196af8cc-f6ad-4ef7-afd1-c45d5231387c\/240\/?1483361727745\">";
+    str += "          <\/span>";
+    str += "          <div class=\"cm-msg-text\">";
+    str += msg;
+    str += "          <\/div>";
+    str += "        <\/div>";
+    $(".chat-logs").append(str);
+    $("#cm-msg-" + INDEX).hide().fadeIn(300);
+    if (type == 'self') {
+      $("#chat-input").val('');
+    }
+    $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight }, 1000);
+  }
+
+  function generate_button_message(msg, buttons) {
+    /* Buttons should be object array 
+      [
+        {
+          name: 'Existing User',
+          value: 'existing'
+        },
+        {
+          name: 'New User',
+          value: 'new'
+        }
+      ]
+    */
+    INDEX++;
+    var btn_obj = buttons.map(function (button) {
+      return "              <li class=\"button\"><a href=\"javascript:;\" class=\"btn btn-primary chat-btn\" chat-value=\"" + button.value + "\">" + button.name + "<\/a><\/li>";
+    }).join('');
+    var str = "";
+    str += "<div id='cm-msg-" + INDEX + "' class=\"chat-msg user\">";
+    str += "          <span class=\"msg-avatar\">";
+    str += "            <img src=\"https:\/\/image.crisp.im\/avatar\/operator\/196af8cc-f6ad-4ef7-afd1-c45d5231387c\/240\/?1483361727745\">";
+    str += "          <\/span>";
+    str += "          <div class=\"cm-msg-text\">";
+    str += msg;
+    str += "          <\/div>";
+    str += "          <div class=\"cm-msg-button\">";
+    str += "            <ul>";
+    str += btn_obj;
+    str += "            <\/ul>";
+    str += "          <\/div>";
+    str += "        <\/div>";
+    $(".chat-logs").append(str);
+    $("#cm-msg-" + INDEX).hide().fadeIn(300);
+    $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight }, 1000);
+    $("#chat-input").attr("disabled", true);
+  }
+
+  $(document).delegate(".chat-btn", "click", function () {
+    var value = $(this).attr("chat-value");
+    var name = $(this).html();
+    $("#chat-input").attr("disabled", false);
+    generate_message(name, 'self');
+  })
+
+  $("#chat-circle").click(function () {
+    $("#chat-circle").toggle('scale');
+    $(".chat-box").toggle('scale');
+  })
+
+  $(".chat-box-toggle").click(function () {
+    $("#chat-circle").toggle('scale');
+    $(".chat-box").toggle('scale');
+  })
+
   /*--/ Star ScrollTop /--*/
-  $('.scrolltop-mf').on("click", function() {
+  $('.scrolltop-mf').on("click", function () {
     $('html, body').animate({
       scrollTop: 0
     }, 1000);
@@ -55,7 +148,7 @@
 
   /*--/ Star Scrolling nav /--*/
   var mainNav_height = $('#mainNav').outerHeight() - 22;
-  $('a.js-scroll[href*="#"]:not([href="#"])').on("click", function() {
+  $('a.js-scroll[href*="#"]:not([href="#"])').on("click", function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -81,7 +174,7 @@
   }
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll').on("click", function() {
+  $('.js-scroll').on("click", function () {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -94,7 +187,7 @@
 
   /*--/ Navbar Menu Reduce /--*/
   $(window).trigger('scroll');
-  $(window).on('scroll', function() {
+  $(window).on('scroll', function () {
     var pixels = 50;
     var top = 1200;
     if ($(window).scrollTop() > pixels) {
@@ -147,10 +240,21 @@
   });
 
   // Initiate venobox (lightbox feature used in portofilo)
-  $(document).ready(function() {
+  $(document).ready(function () {
     $('.venobox').venobox({
       'share': false
     });
+  });
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  }
+  $(window).on('load', function () {
+    aos_init();
   });
 
 })(jQuery);
